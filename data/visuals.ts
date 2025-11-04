ec1b98f1031f3c151d8afa2fd4c822fadc6ce35e
qaramanis <127772424+qaramanis@users.noreply.github.com>
2025-11-04 15:03:34 +0200

    data update

--- data/visuals.ts
+++ data/visuals.ts
@@ -2,58 +2,75 @@ export interface Visual {
   id: string;
   title: string;
   imagePath: string;
+  dimensions: {
+    width: number;
+    height: number;
+  };
   description?: string;
 }
 
 export const visuals: Visual[] = [
   {
     id: "1",
-    title: "Visual 1",
-    imagePath: "/visuals/1A514541-8DBC-454E-8AF1-51909137BBA2_4_5005_c.jpeg",
+    title: "Abstract",
+    imagePath: "/visuals/landing-page/abstract.jpg",
+    dimensions: {
+      width: 1122,
+      height: 1650,
+    },
   },
   {
     id: "2",
-    title: "Visual 2",
-    imagePath: "/visuals/30666A10-E6C4-4A7C-9455-3F146252D26C_4_5005_c.jpeg",
+    title: "Birds 1",
+    imagePath: "/visuals/landing-page/birds-1.jpg",
+    dimensions: {
+      width: 1592,
+      height: 2048,
+    },
   },
   {
     id: "3",
-    title: "Visual 3",
-    imagePath: "/visuals/3B2DABAB-C091-4838-81BD-2ECA59511613.jpeg",
+    title: "Collage",
+    imagePath: "/visuals/landing-page/collage.jpeg",
+    dimensions: {
+      width: 1202,
+      height: 653,
+    },
   },
   {
     id: "4",
-    title: "Visual 4",
-    imagePath: "/visuals/44029C69-EE11-4E67-90BE-9EF60367794D_1_105_c.jpeg",
+    title: "Copy 1",
+    imagePath: "/visuals/landing-page/copy-1.jpg",
+    dimensions: {
+      width: 2048,
+      height: 1536,
+    },
   },
   {
     id: "5",
-    title: "Visual 5",
-    imagePath: "/visuals/5AFBFF51-9FE0-4966-9062-6BC69AF98D64.jpeg",
+    title: "Horse",
+    imagePath: "/visuals/landing-page/horse.jpg",
+    dimensions: {
+      width: 2048,
+      height: 1605,
+    },
   },
   {
     id: "6",
-    title: "Visual 6",
-    imagePath: "/visuals/5EB4CC4C-A4D8-4B36-9D09-A366DEC5FC73_1_105_c.jpeg",
+    title: "Human",
+    imagePath: "/visuals/landing-page/human.jpg",
+    dimensions: {
+      width: 960,
+      height: 1200,
+    },
   },
   {
     id: "7",
-    title: "Visual 7",
-    imagePath: "/visuals/6613785C-7A1D-4020-AD19-B603478595F7.jpeg",
-  },
-  {
-    id: "8",
-    title: "Visual 8",
-    imagePath: "/visuals/D9928A02-DD1B-41C0-8871-C002519555BA_1_105_c.jpeg",
-  },
-  {
-    id: "9",
-    title: "Visual 9",
-    imagePath: "/visuals/E6C9B13B-422F-4E59-9335-77EA2A693369_1_105_c.jpeg",
-  },
-  {
-    id: "10",
-    title: "Visual 10",
-    imagePath: "/visuals/F40CC247-F953-43FD-90D8-4A16C3E27031_1_102_o.jpeg",
+    title: "Stuck",
+    imagePath: "/visuals/landing-page/stuck.jpg",
+    dimensions: {
+      width: 2048,
+      height: 1536,
+    },
   },
 ];
